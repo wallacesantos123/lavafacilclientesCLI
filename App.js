18a0faa1517e5f35bfe144149a2a91d5ee260b80
Wallace Santos <dalas1998.ws@gmail.com>
2022-05-19 22:55:56 -0300

    Update 19/05/2022

--- App.js
+++ App.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import Navegacao from './src/Navegacao.js';
 import Menu from './src/paginas/menu/Menu.js';
+import { enableLatestRenderer } from 'react-native-maps';
+
+enableLatestRenderer();
 
 const App = () => {
   return (
