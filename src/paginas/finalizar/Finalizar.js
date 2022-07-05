12357b65893ea953838c0af21a33683a1e7b1511
Wallace Santos <dalas1998.ws@gmail.com>
2022-07-05 17:15:54 -0300

    Update Finalizar.js

--- src/paginas/finalizar/Finalizar.js
+++ src/paginas/finalizar/Finalizar.js
@@ -1,23 +1,60 @@
 import React from "react";
-import { KeyboardAvoidingView, Text, View } from 'react-native';
+import { KeyboardAvoidingView, Text, View, StyleSheet } from 'react-native';
+import { TouchableOpacity } from "react-native-gesture-handler";
 
 const Finalizar = () => {
     return(
         <KeyboardAvoidingView>
             <View>
-                <Text>Confirmação de Lavagem</Text>
-                <Text>Lava-Rapido: Suave na Nave</Text>
-                <Text>Lavagem: Ducha</Text>
-                <Text>aspiracão: Não</Text>
-                <Text>Pretinho: Não</Text>
-                <Text>Produto: Não</Text>
-                <Text>Motor: Não</Text>
-                <Text>Valor: R$10,00</Text>
-                <Text>Forma de Pagamento: Dinheiro</Text>
-                <Text>Confirmar e ir para o Lava-Rapido</Text>
+                <Text style={Style.titulo}>Confirmação de Lavagem</Text>
+                <Text style={Style.opcoes}>Lava-Rapido: Suave na Nave</Text>
+                <Text style={Style.opcoes}>Lavagem: Ducha</Text>
+                <Text style={Style.opcoes}>Aspiracão: Não</Text>
+                <Text style={Style.opcoes}>Pretinho: Não</Text>
+                <Text style={Style.opcoes}>Produto: Não</Text>
+                <Text style={Style.opcoes}>Motor: Não</Text>
+                <Text style={Style.opcoes}>Valor: R$10,00</Text>
+                <Text style={Style.opcoes}>Forma de Pagamento: Dinheiro</Text>
+
+                <TouchableOpacity 
+                    style={Style.confirm}
+                    onPress={() => {console.log('Confirmou!!!')}}
+                >
+                    <Text style={Style.txtConfirm}>Confirmar e ir Para o Lava-Rapido</Text>
+                </TouchableOpacity>
             </View>
         </KeyboardAvoidingView>
     )
 }
+
+const Style = new StyleSheet.create({
+    titulo: {
+        textAlign: 'center',
+        fontWeight: 'bold',
+        fontSize : 20
+    },
+
+    opcoes: {
+        textAlign: 'center',
+        fontWeight: 'bold',
+        fontSize: 14
+    },
+    
+    confirm: {
+        width: '80%',
+        height: '35%',
+        borderWidth: 2,
+        borderColor: '#000',
+        alignSelf: 'center',
+        alignItems: 'center',
+        justifyContent: 'center'
+        
+    },
+
+    txtConfirm: {
+        fontWeight: 'bold',
+        fontSize: 18
+    }
+})
 
 export default Finalizar;
